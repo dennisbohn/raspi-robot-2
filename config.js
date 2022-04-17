6d0bf7dad90b55522b419f8cebb6552b3b3df697
dennisbohn <dennis.bohn@bohnmedia.de>
2022-04-17 18:38:10 +0200

    Parameter in Configfile ausgelagert

---
+++ config.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+
+// Arguments
+const action = process.argv[2];
+const key = process.argv[3];
+const value = process.argv[4];
+
+// Config
+const configFile = __dirname + "/config.json";
+const config = fs.existsSync(configFile)
+  ? JSON.parse(fs.readFileSync(configFile).toString())
+  : {};
+const saveConfig = function () {
+  fs.writeFileSync(configFile, JSON.stringify(config, null, 4));
+};
+
+// Set
+const set = function (key, value) {
+  if (value === undefined) return;
+  // Server
+  if (["authToken", "hostname"].includes(key)) {
+    config.server = config.server || {};
+    config.server[key] = value;
+    saveConfig();
+  }
+  // Libcamera
+  if (["width", "height", "framerate", "bitrate"].includes(key)) {
+    config.libcamera = config.libcamera || {};
+    config.libcamera[key] = parseInt(value) || 0;
+    saveConfig();
+  }
+  if (key === "libcamera" && value === "defaults") {
+    config.libcamera = {
+      width: 640,
+      height: 360,
+      framerate: 30,
+      bitrate: 1000000,
+    };
+    saveConfig();
+  }
+};
+
+// Action
+if (action === "set") set(key, value);
--- robot.js
+++ robot.js
@@ -1,32 +1,25 @@
-const readFileSync = require("fs").readFileSync;
+const fs = require("fs");
 const Motors = require("./js/Motors.js");
 const VideoBuffer = require("./js/VideoBuffer.js");
 const io = require("socket.io-client");
-const authToken = readFileSync(".authToken").toString().trim();
-const socket = io("wss://beta.robot.bohn.media/", {
-  auth: { token: authToken },
+const config = JSON.parse(fs.readFileSync("config.json").toString());
+const socket = io("wss://" + config.server.hostname + "/", {
+  auth: { token: config.server.authToken },
 });
 const videoBuffer = new VideoBuffer();
 const spawn = require("child_process").spawn;
 const pythonProcess = spawn("python3", ["-u", "./js/Motors.py"]);
 const motors = new Motors(pythonProcess);
-const Raspivid = require("./js/Raspivid.js");
+const Libcamera = require("./js/Libcamera.js");
 
 // Raspberry pi camera
-const raspivid = new Raspivid({
-  width: 640,
-  height: 360,
-  framerate: 30,
-  profile: "baseline",
-  timeout: 0,
-  bitrate: 1000000,
-});
+const libcamera = new Libcamera(config.libcamera);
 
 // Send all chunks to the videobuffer
-raspivid.on("data", videoBuffer.pushVideoChunk);
+libcamera.on("data", videoBuffer.pushVideoChunk);
 
 // Start camera
-raspivid.start();
+libcamera.start();
 
 // Connect to websocket
 socket.on("connect", () => {
@@ -38,6 +31,6 @@ socket.on("motors", (data) => {
 });
 
 socket.on("camera", function (value) {
-  if (value === "start") raspivid.start();
-  if (value === "stop") raspivid.stop();
+  if (value === "start") libcamera.start();
+  if (value === "stop") libcamera.stop();
 });
