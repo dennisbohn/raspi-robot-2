9931fcc47dff03efd7404b686b844aef659a0b45
dennisbohn <dennis.bohn@bohnmedia.de>
2022-04-17 19:18:17 +0200

    robots.js aufgeräumt

--- robot.js
+++ robot.js
@@ -1,3 +1,11 @@
+// Modules
+const Motors = require("./js/Motors.js");
+const VideoBuffer = require("./js/VideoBuffer.js");
+const Libcamera = require("./js/Libcamera.js");
+const fs = require("fs");
+const io = require("socket.io-client");
+const ChildProcess = require("child_process");
+
 // Load config
 const configFile = __dirname + "/config.json";
 if (!fs.existsSync(configFile)) process.exit();
@@ -5,20 +13,19 @@ const config = JSON.parse(fs.readFileSync("config.json").toString());
 if (!config.server || !config.server.hostname || !config.server.authToken)
   process.exit();
 
-const fs = require("fs");
-const Motors = require("./js/Motors.js");
-const VideoBuffer = require("./js/VideoBuffer.js");
-const io = require("socket.io-client");
+// Connect to server
 const socket = io("wss://" + config.server.hostname + "/", {
   auth: { token: config.server.authToken },
 });
-const videoBuffer = new VideoBuffer();
-const spawn = require("child_process").spawn;
-const pythonProcess = spawn("python3", ["-u", "./js/Motors.py"]);
-const motors = new Motors(pythonProcess);
-const Libcamera = require("./js/Libcamera.js");
 
-// Raspberry pi camera
+// Create video buffer for output stream
+const videoBuffer = new VideoBuffer();
+
+// Start motors
+const pythonProcess = ChildProcess.spawn("python3", ["-u", "./js/Motors.py"]);
+const motors = new Motors(pythonProcess);
+
+// Start camera
 const libcamera = new Libcamera(config.libcamera);
 
 // Send all chunks to the videobuffer
@@ -32,10 +39,12 @@ socket.on("connect", () => {
   videoBuffer.addReceiverSocket(socket);
 });
 
+// Motor controlls
 socket.on("motors", (data) => {
   motors.setMotors(data);
 });
 
+// Camera controlls
 socket.on("camera", function (value) {
   if (value === "start") libcamera.start();
   if (value === "stop") libcamera.stop();
