bd9583c6f53fd4ee88a84de4472657e3dde8655f
dennisbohn <dennis.bohn@bohnmedia.de>
2022-04-17 18:51:33 +0200

    Updatescript erzeugt

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# stop robot
+pm2 stop robot
+
+# goto home directory
+cd $HOME
+
+# backup config file
+mv ./raspi-robot-2/config.json ./
+
+# delete old version
+sudo rm -R ./raspi-robot-2
+
+# clone new version from repo
+git clone https://github.com/dennisbohn/raspi-robot-2.git
+
+# move config file back to raspi-robot-2 folder
+mv ./config.json ./raspi-robot-2
+
+# start robot
+pm2 start robot
