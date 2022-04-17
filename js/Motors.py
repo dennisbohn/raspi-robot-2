73f798609a244f790c06f4b17b2ff9e1bcb37ad6
dennisbohn <dennis.bohn@bohnmedia.de>
2022-04-17 18:38:42 +0200

    Motoren im Falle eines Stops ausschalten

--- js/Motors.py
+++ js/Motors.py
@@ -13,13 +13,17 @@ motor = {
 }
 pattern = re.compile("^[AB]\-?\d+$")
 
-print("start")
 for line in sys.stdin:
-    print(line.strip())
     splits = line.strip().upper().split()
     for split in splits:
         if (re.match(pattern, split)):
             wheel = split[0:1]
             speed = int(split[1:])
             motor[wheel] = speed
-    pair.start(motor["A"], motor["B"] * -1)
+    if motor["A"] == 0 and motor["B"] == 0:
+        print("stop")
+        pair.start(0, 0)
+        pair.stop()
+    else:
+        print(line.strip())
+        pair.start(motor["A"], motor["B"] * -1)
